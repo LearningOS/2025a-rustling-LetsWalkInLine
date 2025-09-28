927e5ef150e39f301a5317a8354a569b5c3ca00e
LetsWalkInLine <1327604853@qq.com>
2025-09-28 18:55:48 +0800

    fin again

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -4,11 +4,26 @@
 // hint.
 
 
+// fn main() {
+//     let mut res = 42;
+//     let option = Some(12);
+//     while let Some(x) = option {
+//         res += x;
+//     }
+//     println!("{}", res);
+// }
+
+use std::f32;
+use std::f32::consts::PI;
+
 fn main() {
-    let mut res = 42;
-    let option = Some(12);
-    while let Some(x) = option {
-        res += x;
-    }
-    println!("{}", res);
+    let pi = PI;
+    let radius = 5.00f32;
+
+    let area = pi * f32::powi(radius, 2);
+
+    println!(
+        "The area of a circle with radius {:.2} is {:.5}!",
+        radius, area
+    )
 }
